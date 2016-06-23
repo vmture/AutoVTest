8050a6dbfd677d620c7e03f20cf01d74a1926945
vmture <vmture@gmail.com>
2016-06-23 19:10:51 +0800

    changes log

--- vpntest/start.py
+++ vpntest/start.py
@@ -4,22 +4,30 @@ __author__ = 'dengzihong'
 import os
 from appium import webdriver
 import time
-# import unittest
-# class VpnTest(unittest.TestCase):
-def setUp(self):
-    'start appium test'
-    desired_caps = {}
-# desired_caps['appium-version'] = '1.0'
-    desired_caps['platformName'] = 'Android'
-    desired_caps['platformVersion'] = '5.1'
-    desired_caps['deviceName'] = '810BBM422E25'
-    desired_caps['appPackage'] = 'com.lantern.safecommand'
-    desired_caps['appActivity'] = '.act.WelcomeAct'
-# desired_caps['app'] = os.path.abspath('/Users/Vmture/Desktop/SafeVpn_20160309092542_2117_47_debug.apk')
-    self['driver']=webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
-    # driver.shake()
-    # driver.press(2,3)
-    # driver.implicitly_wait(60)
-def quit(self):
-    self['driver'].quit()
+# import unittest(unittest.TestCase)
+class VpnTest():
 
+
+    def setUp(self):
+        'start appium test'
+        desired_caps = {}
+        # desired_caps['appium-version'] = '1.0'
+        desired_caps['platformName'] = 'Android'
+        desired_caps['platformVersion'] = '5.1'
+        desired_caps['deviceName'] = '810BBM422E25'
+        desired_caps['appPackage'] = 'com.lantern.safecommand'
+        desired_caps['appActivity'] = '.act.WelcomeAct'
+        # desired_caps['app'] = os.path.abspath('/Users/Vmture/Desktop/SafeVpn_20160309092542_2117_47_debug.apk')
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
+        # driver.shake()
+        # driver.press(2,3)
+        # driver.implicitly_wait(60)
+
+
+    def quit(self):
+        self.driver.quit()
+
+if __name__ == '__main__':
+    self=VpnTest()
+    self.setUp()
+    self.quit()

fe7256d11b20a9c60dfe27d73da1567b825c5d1f
vmture <vmture@gmail.com>
2016-06-27 20:25:40 +0800

    changes log

---
+++ ZhiHuTest/devices.py
@@ -0,0 +1,11 @@
+__author__ = 'vmture'
+
+Emulator = {'platformName':'Android',
+            'platformVersion':'4.4.4',
+            'deviceName':'Android Emulator',
+            'appPackage':'com.zhihu.android',
+            'appActivity':'com.zhihu.android.app.ui.activity.MainActivity'}
+
+Package = {}
+
+LoginActivity = {}
--- ZhiHuTest/start.py
+++ ZhiHuTest/start.py
@@ -3,17 +3,19 @@ __author__ = 'vmture'
 import time
 import os
 import HTMLTestRunner
+import devices
 from appium import webdriver
 from time import sleep
 import unittest
 class ZhihuTest(unittest.TestCase):
     def setUp(self):
         desired_caps={}
-        desired_caps['platformName'] = 'Android'
-        desired_caps['platformVersion'] = '4.4.4'
-        desired_caps['deviceName'] = 'Android Emulator'
-        desired_caps['appPackage'] = 'com.zhihu.android'
-        desired_caps['appActivity'] = 'com.zhihu.android.app.ui.activity.MainActivity'
+        desired_caps.update(devices.Emulator)
+        # desired_caps['platformName'] = 'Android'
+        # desired_caps['platformVersion'] = '4.4.4'
+        # desired_caps['deviceName'] = 'Android Emulator'
+        # desired_caps['appPackage'] = 'com.zhihu.android'
+        # desired_caps['appActivity'] = 'com.zhihu.android.app.ui.activity.MainActivity'
         desired_caps['app'] = os.path.abspath('C:\\Users\\dengzihong.ZENMEN\\AutoVTest\\ZhiHuTest\\app\\知乎.apk')
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
@@ -29,8 +31,10 @@ class ZhihuTest(unittest.TestCase):
         a1 = self.driver.find_element_by_id("com.zhihu.android:id/login_btn")
         self.assertEqual('登录',(a1.text).encode("UTF-8"))
         a1.click()
+        print(u"\n弹出登录框")
         sleep(5)
 
+    def test2(self):
         a2 = self.driver.find_element_by_id("com.zhihu.android:id/title")
         self.assertEqual('登录',(a2.text).encode("UTF-8"))
 
@@ -54,16 +58,21 @@ class ZhihuTest(unittest.TestCase):
         self.assertEqual('搜索话题、问题或人',(a6.text).encode("UTF-8"))
         print(u'登录成功')
 
+    def login(self):
+        ZhihuTest.test1(self)
+        ZhihuTest.test2(self)
+
 if __name__ == '__main__':
     # suite = unittest.TestLoader().loadTestsFromTestCase(ZhihuTest)
     # unittest.TextTestRunner(verbosity=2).run(suite)
     # unittest.main()
     # testunit = unittest.makeSuite(ZhihuTest)
-    testunit = unittest.TestSuite()
-    testunit.addTest(ZhihuTest('test1'))
     time = time.strftime('%Y_%m_%d_%H_%M',time.localtime(time.time()))
     html = time+'.html'
     filename = 'C:\\Users\\dengzihong.ZENMEN\\AutoVTest\\ZhiHuTest\\report\\test_report'+html
     fp = file(filename,'wb')
-    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title='test_report',description='Report_description')
+
+    testunit = unittest.TestSuite()
+    testunit.addTest(ZhihuTest('login'))
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title='test_report',description=u'用例执行情况：')
     runner.run(testunit)

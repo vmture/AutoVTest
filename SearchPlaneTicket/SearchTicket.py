454fd0460125e62a5b16be45b7d13bddfade1d06
vmture <vmture@gmail.com>
2016-07-07 17:10:53 +0800

    changes log

--- SearchPlaneTicket/SearchTicket.py
+++ SearchPlaneTicket/SearchTicket.py
@@ -1,29 +1,42 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 __author__ = 'vmture'
 # from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from time import sleep
 from selenium import webdriver
-class Search_Ticket:
-    def SHA_SZX(self):
-        SHA_SZX_URL=[
-            'https://sjipiao.alitrip.com/flight_search_result.htm?spm=181.7091613.a1z67.1001&searchBy=1280&tripType=0&depCityName=%C9%CF%BA%A3&depCity=SHA&depDate=2016-09-06&arrCityName=%C9%EE%DB%DA&arrCity=SZX&arrDate=']
-            # 'http://flights.ctrip.com/booking/SHA-SZX-day-1.html?DDate1=2016-09-10',
-            # 'http://flights.ctrip.com/booking/SHA-SZX-day-1.html?DDate1=2016-09-11']
 
-        for url in SHA_SZX_URL:
-            service_args = ['--proxy=localhost:9999', '--proxy-type=socks5', ]
-            driver =webdriver.PhantomJS(executable_path="phantomjs.exe",service_args=service_args)
+
+class Search_Ticket:
+    def __init__(self):
+        SHA = 'SHA'
+        SHA_NUM = '%C9%CF%BA%A3'
+        SZX = 'SZX'
+        SZX_NUM = '%C9%EE%DB%DA'
+        self.SHA_SZX_URL = []
+        self.Date = ['2016-09-10', '2016-09-11', '2016-09-12']
+        for i in self.Date:
+            self.SHA_SZX_URL.append(
+                'https://sjipiao.alitrip.com/flight_search_result.htm?spm=181.7091613.a1z67.1001&searchBy=1280&tripType=0&depCityName=' + SZX_NUM + '&depCity=' + SZX + '&depDate=' + i + '&arrCityName=' + SHA_NUM + '&arrCity=' + SHA + '&arrDate=')
+
+    def SHA_SZX(self):
+        # SHA_SZX_URL=[
+        # 'https://sjipiao.alitrip.com/flight_search_result.htm?spm=181.7091613.a1z67.1001&searchBy=1280&tripType=0&depCityName=%C9%CF%BA%A3&depCity=SHA&depDate=2016-09-06&arrCityName=%C9%EE%DB%DA&arrCity=SZX&arrDate=']
+        #     # 'http://flights.ctrip.com/booking/SHA-SZX-day-1.html?DDate1=2016-09-10',
+        #     # 'http://flights.ctrip.com/booking/SHA-SZX-day-1.html?DDate1=2016-09-11']
+        num = 0
+        for url in self.SHA_SZX_URL:
+            # service_args = ['--proxy=localhost:9999', '--proxy-type=socks5', ]
+            # driver =webdriver.PhantomJS(executable_path="phantomjs.exe",service_args=service_args)
             # driver = webdriver.PhantomJS(executable_path="phantomjs.exe")
-            # driver = webdriver.Chrome()
-            # driver.maximize_window()
+            driver = webdriver.Chrome()
+            driver.maximize_window()
             driver.get(url)
             # sleep(2)
             # button = driver.find_element_by_id('ks-overlay-close-ks-component1015')
             # button.click()
             sleep(10)
             html = driver.page_source
-            driver.get_screenshot_as_file('WebPage.jpg')
+            driver.get_screenshot_as_file(num+'WebPage.jpg')
             driver.quit()
             bs_obj = BeautifulSoup(html, 'html.parser')
             # print(bs_obj)
@@ -35,29 +48,35 @@ class Search_Ticket:
             Arrive_Place = []
             Ticket_fee = []
 
-            for i in bs_obj.find_all('td', {'class':'flight-line'}):
+            for i in bs_obj.find_all('td', {'class': 'flight-line'}):
                 PlaneCompany.append(i.get_text())
-            for j in bs_obj.find_all('p', {'class':'flight-time-deptime'}):
+            for j in bs_obj.find_all('p', {'class': 'flight-time-deptime'}):
                 Beign_time.append(j.get_text())
-            for k in bs_obj.find_all('span', {'class':'s-time'}):
+            for k in bs_obj.find_all('span', {'class': 's-time'}):
                 Arrive_time.append(k.get_text())
-            for l in bs_obj.find_all('p', {'class':'port-dep'}):
+            for l in bs_obj.find_all('p', {'class': 'port-dep'}):
                 Beign_Place.append(l.get_text())
-            for m in bs_obj.find_all('p', {'class':'port-arr'}):
+            for m in bs_obj.find_all('p', {'class': 'port-arr'}):
                 Arrive_Place.append(m.get_text())
-            for n in bs_obj.find_all('td', {'class':'flight-price J_FlightPriceWrap'}):
+            for n in bs_obj.find_all('td', {'class': 'flight-price J_FlightPriceWrap'}):
                 Ticket_fee.append(n.get_text())
             # print(Beign_time)
             # print(Arrive_time)
+            try:
+                print('SZX  ---->  SHA  ' + self.Date[num] + ': \n' )
+                num += 1
+            except:
+                pass
             for num in range(len(PlaneCompany)):
                 print(u'航班:  ' + PlaneCompany[num])
-                print(u'起飞时间:  '+ Beign_time[num])
-                print(u'降落时间:  '+ Arrive_time[num])
-                print(u'起飞地点:  '+ Beign_Place[num])
-                print(u'降落地点:  '+ Arrive_Place[num])
-                print(u'票价:  '+ Ticket_fee[num] + '\n')
+                print(u'起飞时间:  ' + Beign_time[num])
+                print(u'降落时间:  ' + Arrive_time[num])
+                print(u'起飞地点:  ' + Beign_Place[num])
+                print(u'降落地点:  ' + Arrive_Place[num])
+                print(u'票价:  ' + Ticket_fee[num] + '\n')
 
 
 if __name__ == '__main__':
     self = Search_Ticket()
+    self.__init__()
     self.SHA_SZX()
